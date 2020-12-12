73295a47a8912574703fdf6bd36fa100b22a35b0
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2020-12-12 20:59:53 +0100

    added winning and draw moves to the file

--- bin/main.rb
+++ bin/main.rb
@@ -18,62 +18,48 @@ game_system = true
 
 i = 0
 
-while game_system
-  space_count = nil
-
+  puts
   puts ' 1 2 3'
-  puts '-------------'
+  puts "-------------"
   puts "| #{layout[0]} | #{layout[1]} | #{layout[2]} |"
-  puts '-------------'
+  puts "-------------"
   puts "| #{layout[3]} | #{layout[4]} | #{layout[5]} |"
-  puts '-------------'
+  puts "-------------"
   puts "| #{layout[6]} | #{layout[7]} | #{layout[8]} |"
-  puts '-------------'
+  puts "-------------"
 
+while game_system
   if i != 9
     puts 'Note: Game is over when all squares are filled.'
     puts "Round for #{players[i % 2]}"
-    puts 'Please enter a number (from 1 to 9) to play your move.'
+    puts 'Place your token (X or O) on positions 1-9 on the board'
     input = gets.chomp
   end
 
-  if input.length == 1
-    space_count = if input.include?('1')
-                    0
-                  elsif input.include?('2')
-                    1
-                  elsif input.include?('3')
-                    2
-                  elsif input.include?('4')
-                    3
-                  elsif input.include?('5')
-                    4
-                  elsif input.include?('6')
-                    5
-                  elsif input.include?('7')
-                    6
-                  elsif input.include?('8')
-                    7
-                  elsif input.include?('9')
-                    8
-                  end
-    if !space_count.nil?
-      if layout[space_count] != 'x' && layout[space_count] != 'o'
-        layout[space_count] = i.even? ? 'x' : 'o'
-        game_system = false if i == 9
-        i += 1
-      else
-        puts 'This square is already filled! Please try again!'
-        game_system = false
-      end
-    else
-      puts 'Wrong value entered! Please try again!'
-      puts
-    end
-  else
-    puts 'Wrong value entered! Please try again!'
+  if input == 'x'
     puts
+    puts <<-LAYOUT
+      | X | O | X |
+      | X | O | X |
+      | X | O | X |
+    LAYOUT
+    puts
+    puts 'valid move'
+    puts 'you won'
+    game_system = false
+  elsif input == 'o'
+    puts
+        puts <<-LAYOUT
+      | X | O | X |
+      | X | O | o |
+      | o | x | X |
+    LAYOUT
+    puts 'valid move'
+    puts 'you draw'
+    game_system = false
+  else
+    puts 'invalid move'
+    # game_system = false
   end
-  puts 'Game Over'
 end
 # rubocop:enable Metrics/BlockNesting
